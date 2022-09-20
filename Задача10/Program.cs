641f5a580434ec6d0be9fb2636e2035bbd8d0b32
Eugeny1978 <eugenytsypyshev@rambler.ru>
2022-09-20 11:56:59 +0500

    Домашнее задание2

---
+++ Задача10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трёхзначное число: ");
+
+int GetNumberTwo(int Number)
+{
+    return (Number/10)%10;
+}
+
+int InputNumber = Convert.ToInt32(Console.ReadLine());
+if (InputNumber/100 == 0)
+{
+    Console.WriteLine("Некорректно введенное число");
+}
+else
+{
+    Console.WriteLine(Math.Abs(GetNumberTwo(InputNumber)));
+}
